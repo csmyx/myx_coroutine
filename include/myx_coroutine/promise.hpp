2095ca03ad5a181f69a6001016116d3504ce1402
Hugo Ma <csmyx@mail.ustc.edu.cn>
2025-10-09 04:22:19 +0800

    1. feat: impl promise, add promise<void> support. 2. test: add tests for `Promise`. 3. chore: add -fsanitize=thread to detect data race.

--- include/myx_coroutine/promise.hpp
+++ include/myx_coroutine/promise.hpp
@@ -5,39 +5,68 @@
 #include <cstdint>
 #include <exception>
 #include <future>
+#include <gmock/gmock.h>
 #include <memory>
 #include <mutex>
 #include <optional>
 #include <spdlog/spdlog.h>
+#include <stdexcept>
 #include <type_traits>
 #include <variant>
 
 namespace myx_coroutine {
 
+// Unit plays the role of a simplest type in case we couldn't
+// use void directly.
+//
+// User shouldn't use this directly.
+struct Unit {
+    constexpr bool operator==(const Unit &) const { return true; }
+
+    constexpr bool operator!=(const Unit &) const { return false; }
+};
+
 /// State shared by Promise and Future.
 template<typename T>
+    requires(!std::is_void_v<T>)
 class FutureState {
-    std::optional<T> value_;
-    std::exception_ptr exception_;
+  public:
+    using ValueT = std::variant<std::monostate, T, std::exception_ptr>;
+
+  private:
+    ValueT value_;
     std::atomic<int8_t> ref_cnt_;
-    std::mutex mtx_;
-    std::condition_variable cv_;
+    mutable std::mutex mtx_;
+    mutable std::condition_variable cv_;
 
   public:
     ~FutureState() {}
 
-    void increment_ref() { ref_cnt_.fetch_add(1, std::memory_order_relaxed); }
+    void increment_ref() noexcept { ref_cnt_.fetch_add(1, std::memory_order_relaxed); }
 
     void decrement_ref() {
-        int cnt = ref_cnt_.fetch_sub(1, std::memory_order_acq_rel);
-        SPDLOG_INFO("cnt: {}", cnt);
-        assert(cnt > 0);
-        if (cnt == 1) {
+        int cnt = ref_cnt_.fetch_sub(1, std::memory_order_acq_rel) - 1;
+        assert(cnt >= 0);
+        if (cnt == 0) {
             delete this;
         }
     }
 
-    void wait() {
+    void detach_promise() {
+        int cnt = ref_cnt_.fetch_sub(1, std::memory_order_acq_rel) - 1;
+        // The call to has_value here in not inside a lock region, which is totally fine.
+        // Because all potential concurent operations from the thread of `Future` are read only.
+        if (!has_value() && cnt > 0) {
+            // If value has not been set yet, and there is also a future, then throw error.
+            try {
+                throw std::runtime_error("Promise is broken");
+            } catch (...) {
+                set_exception(std::current_exception());
+            }
+        }
+    }
+
+    void wait() const {
         std::unique_lock lock(mtx_);
         cv_.wait(lock, [this]() { return is_ready(); });
     }
@@ -54,34 +83,38 @@ class FutureState {
         cv_.wait_until(lock, timeout, [this]() { return is_ready(); });
     }
 
-    void get(std::variant<std::monostate, T, std::exception_ptr> &result) {
+    void get(ValueT &result) const {
         std::unique_lock lock(mtx_);
         assert(is_ready());
-        if (has_exception()) [[unlikely]] {
-            result.template emplace<std::exception_ptr>(exception_);
-            return;
-        }
-        result.template emplace<T>(value_.value());
+        result = value_;
     }
 
     void set_value(T &&value) {
         std::unique_lock lock(mtx_);
         assert(!is_ready());
         if constexpr (std::is_lvalue_reference_v<T>) {
-            value_ = value;
+            value_.template emplace<T>(value);
         } else {
-            value_ = std::move(value);
+            value_.template emplace<T>(std::move(value));
         }
+        cv_.notify_one();
+    }
+
+    void set_value()
+        requires(std::is_same_v<T, Unit>)
+    {
+        std::unique_lock lock(mtx_);
+        assert(!is_ready());
+        value_.template emplace<T>(Unit());
         cv_.notify_one();
     }
 
     void set_exception(std::exception_ptr exception) {
         std::unique_lock lock(mtx_);
         assert(!is_ready());
-        exception_ = exception;
+        value_.template emplace<std::exception_ptr>(exception);
+        cv_.notify_one();
     }
-
-    void get_exception() { return; }
 
     static FutureState *Init() { return new FutureState(); }
 
@@ -89,23 +122,31 @@ class FutureState {
     // Set to private to Prevent constructing FutureState from stack.
     FutureState() : ref_cnt_(1) {}
 
-    bool has_value() { return value_.has_value(); }
+    bool has_value() const noexcept { return std::holds_alternative<T>(value_); }
 
-    bool has_exception() { return exception_ != nullptr; }
+    bool has_exception() const noexcept {
+        return std::holds_alternative<std::exception_ptr>(value_);
+    }
 
-    bool is_ready() { return has_value() || has_exception(); }
+    bool is_ready() const noexcept { return !std::holds_alternative<std::monostate>(value_); }
 };
 
 template<typename T>
-class Future {
-    FutureState<T> *state_;
+class Promise;
 
-  public:
-    Future(FutureState<T> *state) : state_{state} {
+template<typename T>
+class Future {
+    using value_type = std::conditional_t<std::is_void_v<T>, Unit, T>;
+    FutureState<value_type> *state_;
+
+    Future(FutureState<value_type> *state) : state_{state} {
         if (state_) {
             state_->increment_ref();
         }
     }
+
+  public:
+    friend Promise<T>;
 
     ~Future() {
         if (state_) {
@@ -125,54 +166,94 @@ class Future {
         state_->wait_until(timeout);
     }
 
-    T get() {
+    T get() const {
         wait();
-        std::variant<std::monostate, T, std::exception_ptr> result;
+        typename FutureState<value_type>::ValueT result;
         state_->get(result);
         if (std::holds_alternative<std::exception_ptr>(result)) [[unlikely]] {
             std::rethrow_exception(std::get<std::exception_ptr>(result));
         }
-        if (!std::holds_alternative<T>(result)) [[unlikely]] {
+        if (!std::holds_alternative<value_type>(result)) [[unlikely]] {
             throw std::logic_error("get bad value");
         }
-        return std::get<T>(result);
+        if constexpr (!std::is_void_v<T>) {
+            return std::get<value_type>(result);
+        }
     }
 };
 
 template<typename T>
 class Promise {
-    FutureState<T> *state_;
-    bool has_future = false;
+    using value_type = std::conditional_t<std::is_void_v<T>, Unit, T>;
+    FutureState<value_type> *state_;
+    bool has_future_ = false;
 
   public:
-    Promise() : state_{FutureState<T>::Init()} {}
+    Promise() : state_{FutureState<value_type>::Init()} {}
 
-    Promise(Promise &&rhs) {
-        state_ = rhs.state_;
+    Promise(Promise &&rhs) noexcept : state_(rhs.state_), has_future_(rhs.has_future_) {
         rhs.state_ = nullptr;
-        has_future = rhs.has_future;
+        rhs.has_future_ = false;
+    }
+
+    Promise &operator=(Promise &&rhs) noexcept {
+        if (state_) {
+            state_->detach_promise();
+        }
+        state_ = rhs.state_;
+        has_future_ = rhs.has_future_;
+        rhs.state_ = nullptr;
+        rhs.has_future_ = false;
+        return *this;
     }
 
     Promise(const Promise &rhs) = delete;
     Promise &operator=(const Promise &rhs) = delete;
-    Promise &operator=(Promise &&rhs) = delete;
 
     ~Promise() {
         if (state_) {
-            state_->decrement_ref();
+            state_->detach_promise();
         }
     }
 
     Future<T> get_future() {
-        assert(state_);
-        assert(!has_future);
-        has_future = true;
+        check_state();
+        check_future();
+        has_future_ = true;
         return Future<T>(state_);
     }
 
     template<typename Arg>
-    void set_value(Arg &&value) {
+    void set_value(Arg &&value)
+        requires(!std::is_void_v<T>)
+    {
+        check_state();
         state_->set_value(T(std::forward<Arg>(value)));
+    }
+
+    void set_value()
+        requires(std::is_void_v<T>)
+    {
+        check_state();
+        state_->set_value();
+    }
+
+    void set_exception(std::exception_ptr exception) {
+        check_state();
+        state_->set_exception(exception);
+    }
+
+  private:
+    void check_state() const {
+        if (!state_) {
+            throw std::runtime_error("state is not valid");
+        }
+    }
+
+    void check_future() const {
+        if (has_future_) {
+            throw std::runtime_error("future has already been set");
+        }
     }
 };
 } // namespace myx_coroutine
--- test/test_promise.cpp
+++ test/test_promise.cpp
@@ -3,6 +3,7 @@
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 #include <spdlog/spdlog.h>
+#include <stdexcept>
 #include <thread>
 
 namespace myx_coroutine {
@@ -16,11 +17,10 @@ struct PromiseTest : public testing::Test {
             "[%s:%#]: "            // file:line
             "%v"                   // content
         );
-        SPDLOG_INFO("Hello, spdlog!");
-        SPDLOG_ERROR("An error occurred");
+        SPDLOG_INFO("testing started");
     }
 
-    void TearDown() override {}
+    void TearDown() override { SPDLOG_INFO("testing finished"); }
 };
 
 using namespace std::chrono_literals;
@@ -28,21 +28,158 @@ using namespace std::chrono_literals;
 TEST_F(PromiseTest, simpleTest) {
     Promise<int> p;
     auto f = p.get_future();
-    SPDLOG_INFO("1");
     int x = 100;
     std::thread t(
         [&](Promise<int> promise, int x) {
-            std::this_thread::sleep_for(1s);
+            std::this_thread::sleep_for(500ms);
             promise.set_value(x);
-            SPDLOG_INFO("set value");
         },
         std::move(p), x
     );
-    t.join();
-    SPDLOG_INFO("2");
     auto v = f.get();
-    SPDLOG_INFO("3");
+    t.join();
 
     EXPECT_EQ(v, x);
+}
+
+TEST_F(PromiseTest, setException) {
+    Promise<int> p;
+    auto f = p.get_future();
+    const std::string err_msg = "test exception";
+
+    std::thread t([&p, &err_msg]() {
+        std::this_thread::sleep_for(500ms);
+        try {
+            throw std::runtime_error(err_msg);
+        } catch (...) {
+            p.set_exception(std::current_exception());
+        }
+        SPDLOG_INFO("set exception");
+    });
+
+    t.join();
+    SPDLOG_INFO("waiting for exception");
+
+    // verify catch expection.
+    EXPECT_THROW(
+        {
+            try {
+                f.get();
+            } catch (const std::runtime_error &e) {
+                EXPECT_STREQ(e.what(), err_msg.c_str());
+                throw;
+            }
+        },
+        std::runtime_error
+    );
+}
+
+TEST_F(PromiseTest, promiseDestroyedWithoutValue) {
+    Promise<int> p;
+    auto f = p.get_future();
+
+    std::thread t([p = std::move(p)]() mutable {
+        std::this_thread::sleep_for(500ms);
+        // destroy promise without setting value(broken promise).
+    });
+    t.join();
+
+    EXPECT_THROW(f.get(), std::runtime_error);
+}
+
+TEST_F(PromiseTest, moveSemantics) {
+    Promise<int> p1;
+    auto f1 = p1.get_future();
+    Promise<int> p2(std::move(p1));
+
+    // test move construct
+    EXPECT_THROW(p1.set_value(100), std::runtime_error);
+
+    std::thread t1([p = std::move(p2)]() mutable {
+        std::this_thread::sleep_for(500ms);
+        p.set_value(200);
+    });
+    t1.join();
+    EXPECT_EQ(f1.get(), 200);
+
+    Promise<int> p3;
+    auto f3 = p3.get_future();
+    Promise<int> p4;
+    p4 = std::move(p3);
+
+    // test move assignment
+    EXPECT_THROW(p3.set_value(300), std::runtime_error);
+
+    std::thread t2([p = std::move(p4)]() mutable {
+        std::this_thread::sleep_for(500ms);
+        p.set_value(400);
+    });
+    t2.join();
+    EXPECT_EQ(f3.get(), 400);
+}
+
+// Test promise of void type
+TEST_F(PromiseTest, voidType) {
+    Promise<void> p;
+    auto f = p.get_future();
+    bool flag = false;
+
+    std::thread t([&p, &flag]() {
+        std::this_thread::sleep_for(500ms);
+        flag = true;
+        p.set_value(); // No argument for void type
+        SPDLOG_INFO("void promise set");
+    });
+
+    f.get(); // Wait for completion, no return value
+    t.join();
+    EXPECT_TRUE(flag); // Verify that the sub-thread logic has been executed
+}
+
+// Test calling get_future multiple times (should throw an exception, consistent with std)
+TEST_F(PromiseTest, multipleGetFuture) {
+    Promise<int> p;
+    (void)p.get_future(); // First call is valid
+    // Subsequent calls should throw an exception
+    EXPECT_THROW(p.get_future(), std::runtime_error);
+    EXPECT_THROW(p.get_future(), std::runtime_error);
+}
+
+// Test the blocking waiting behavior of future
+TEST_F(PromiseTest, futureBlockUntilReady) {
+    Promise<std::string> p;
+    auto f = p.get_future();
+    std::string result;
+    std::atomic<bool> thread_started = false;
+
+    std::thread t([&p, &thread_started]() {
+        thread_started = true;
+        std::this_thread::sleep_for(1s); // Simulate time-consuming operation
+        p.set_value("hello future");
+    });
+
+    // Wait for the sub-thread to start
+    while (!thread_started) {
+        std::this_thread::yield();
+    }
+
+    // Time to verify that get() will block until the value is ready
+    auto start = std::chrono::steady_clock::now();
+    result = f.get();
+    auto end = std::chrono::steady_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+
+    t.join();
+    EXPECT_EQ(result, "hello future");
+    EXPECT_GE(duration, 900); // Ensure to wait for at least close to 1 second (allowing some error)
+    SPDLOG_INFO("duration: {}", duration);
+}
+
+// Test multiple futures waiting for the same promise (through shared state)
+TEST_F(PromiseTest, multipleFuturesShareState) {
+    Promise<long> p;
+    auto f1 = p.get_future();
+    // do not allow multiple calls to get_future
+    EXPECT_THROW(auto f2 = p.get_future(), std::runtime_error);
 }
 } // namespace myx_coroutine
